8f7aef5a98589f79a9cd4fed226c450eb2f41617
Payload13 <94011653+Payload13@users.noreply.github.com>
2025-03-03 06:40:59 +0000

    6. Fixed scope of imports. Added mutiple logs to debug file paths

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,13 +3,14 @@
 
 #[macro_use]
 mod epub_backend;
-use epub_backend::{extract_metadata, get_cover_base64}; // Explicitly importing functions
+use epub_backend::{extract_metadata, get_cover_base64, check_storage_path}; // Explicitly importing functions
 
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
             extract_metadata,
-            get_cover_base64
+            get_cover_base64,
+            check_storage_path
         ])
         .run(tauri::generate_context!())
         .expect("error while running Tauri application");
