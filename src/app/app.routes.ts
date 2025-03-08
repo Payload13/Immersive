6f13bb510debada790168036893bc0bea0dba40f
Payload13 <94011653+Payload13@users.noreply.github.com>
2025-03-08 21:02:32 +0000

    7. Fixed epub reader. Added an injectable resolver to handler bookIds from the library to reader

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,28 @@
-import { Routes } from '@angular/router';
-import { LibraryComponent } from './pages/library/library.component';
-import { ReaderComponent } from './pages/reader/reader.component';
-import { ParallelReaderComponent } from './pages/reader/parallel-reader.component';
-import { SplashComponent } from './pages/splash/splash.component';
+import { Routes } from "@angular/router";
+import { LibraryComponent } from "./pages/library/library.component";
+import { ReaderComponent } from "./pages/reader/reader.component";
+import { ParallelReaderComponent } from "./pages/reader/parallel-reader.component";
+import { SplashComponent } from "./pages/splash/splash.component";
+import { BookResolver } from "./resolvers/book.resolver";
 
 export const routes: Routes = [
   {
-    path: '',
-    component: SplashComponent
+    path: "",
+    component: SplashComponent,
   },
   {
-    path: 'library',
-    component: LibraryComponent
+    path: "library",
+    component: LibraryComponent,
   },
   {
-    path: 'read/:bookId',
-    component: ReaderComponent
+    path: "read/:bookId",
+    component: ReaderComponent,
+    resolve: {
+      book: BookResolver,
+    },
   },
   {
-    path: 'parallel/:leftBookId/:rightBookId',
-    component: ParallelReaderComponent
-  }
+    path: "parallel/:leftBookId/:rightBookId",
+    component: ParallelReaderComponent,
+  },
 ];
